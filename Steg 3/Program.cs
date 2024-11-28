772881ba0d4514e6e329e074f4dc1caeda5f7c65
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-28 13:06:16 +0100

    first commit

---
+++ Steg 3/Program.cs
@@ -0,0 +1,10 @@
+﻿static void Square()
+{
+    int number = 0;
+    string pnum = Console.ReadLine();
+    bool success = int.TryParse(pnum, out number);
+    
+    Console.ReadLine();
+}
+Square();
+
